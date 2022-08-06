5b437c57aa8e32bc64ce36eac0f611f47cd35987
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-08-06 00:03:27 -0500

    Removed ico from home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-import Particles from '../containers/Particles'
+import Particles from "../containers/Particles";
 
-const blockchainImg = require('../media/blockchain.png')
-const globeImg = require('../media/globe.png')
-const metaTec = require('../media/MetaTec.PNG')  
+const blockchainImg = require("../media/blockchain.png");
+const globeImg = require("../media/globe.png");
+const metaTec = require("../media/MetaTec.PNG");
 
 const HomeWrapper = styled.div`
   width: 100vw;
   min-height: 100vh;
-`
+`;
 
 const Banner = styled.div`
   position: relative;
@@ -21,7 +21,7 @@ const Banner = styled.div`
   width: 100vw;
   height: 100vh;
   background: linear-gradient(to bottom right, #2b3548, #0d1222);
-`
+`;
 const Banner2 = styled.div`
   position: relative;
   display: flex;
@@ -33,8 +33,8 @@ const Banner2 = styled.div`
   background-image: url(${metaTec});
   background-size: cover;
   background-position: center;
-  border: 3px solid white;
-`
+  border: 3px solid black;
+`;
 
 const Header = styled.div`
   position: absolute;
@@ -46,7 +46,7 @@ const Header = styled.div`
   justify-content: center;
   align-items: center;
   text-align: center;
-`
+`;
 
 const Title = styled.h1`
   margin: 1vw 4vw 1vw 4vw;
@@ -55,7 +55,7 @@ const Title = styled.h1`
   font-size: 3rem;
   border-radius: 50px;
   color: #edf6fe;
-`
+`;
 
 const Subtitle = styled.p`
   margin: 1vw 4vw 1vw 4vw;
@@ -64,12 +64,12 @@ const Subtitle = styled.p`
   max-width: 800px;
   border-radius: 50px;
   color: #e5eef5;
-`
+`;
 
 const Image = styled.img`
-  width: ${props => props.width};
-  height: ${props => props.height};
-`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+`;
 
 const Section1 = styled.div`
   padding: 5vh 15vw 5vh 15vw;
@@ -83,26 +83,26 @@ const Section1 = styled.div`
   }
   background: #202124;
   line-height: 1.5;
-`
+`;
 
 const Column = styled.div`
   align-self: center;
-`
+`;
 
 const Heading = styled.h3`
   text-transform: uppercase;
   color: white;
-`
+`;
 
 const Subheading = styled.p`
   margin-top: 0;
   padding-top: 0;
   color: #a2a7af;
-`
+`;
 
 const Description = styled.p`
   color: #a2a7af;
-`
+`;
 
 const Section2 = styled.div`
   padding: 5vh 15vw 5vh 15vw;
@@ -110,19 +110,19 @@ const Section2 = styled.div`
   grid-template-columns: 1fr 2fr 1fr
   text-align: center;
   background: #202124;
-`
+`;
 
 const ItemHeading = styled.h3`
   margin-bottom: 0;
   padding-bottom: 0;
   color: white;
-`
+`;
 
 const ItemDescription = styled.p`
   margin-top: 0;
   padding-top: 0;
   color: #a2a7af;
-`
+`;
 
 const Section3 = styled.div`
   padding: 5vh 15vw 5vh 15vw;
@@ -130,7 +130,7 @@ const Section3 = styled.div`
   grid-template-columns: 1fr 2fr 1fr
   text-align: center;
   background: #202124;
-`
+`;
 
 const Section4 = styled.div`
   padding: 5vh 15vw 5vh 15vw;
@@ -138,58 +138,41 @@ const Section4 = styled.div`
   grid-template-columns: 1fr 2fr 1fr
   text-align: center;
   background: #202124;
-`
+`;
 
-
-const Home = () =>
+const Home = () => (
   <HomeWrapper>
     <Banner>
       <Particles />
       <Header>
         <Title>Web3 and Blockchain Solution, for Victorian Era Problems</Title>
         <Subtitle>
-          From concept to creation, KronicLabz is dedicated to bring your idea, to reality. 
-          From Blockchain security audits and development, to educational courses and project 
-          creation and management, there's not much we can't help you do.
+          From concept to creation, KronicLabz is dedicated to bring your idea,
+          to reality. From Blockchain security audits and development, to
+          educational courses and project creation and management, there's not
+          much we can't help you do.
         </Subtitle>
       </Header>
     </Banner>
     <Section1>
       <Column>
-        <Image src={blockchainImg} width='30.8vw' height='14.4vw' />
+        <Image src={blockchainImg} width="30.8vw" height="14.4vw" />
       </Column>
       <Column>
         <Heading>Our Mission</Heading>
         <Description>
-          The Web3 space has given birth to a new era of digital technology 
-          that is changing the way we think about and interact with the world.
-          We are committed to providing the best possible service to our
-          customers, and to the future of the Web3 space. KronicLabz is continuously
-          pushing the boundaries of the blockchain and NFTs in particular as a tool 
-          that can help shape our futures. We are a team of developers, designers,           
-          engineers, mothers, and fathers, dedicated to the advancement of the Web3.
+          The Web3 space has given birth to a new era of digital technology that
+          is changing the way we think about and interact with the world. We are
+          committed to providing the best possible service to our customers, and
+          to the future of the Web3 space. KronicLabz is continuously pushing
+          the boundaries of the blockchain and NFTs in particular as a tool that
+          can help shape our futures. We are a team of developers, designers,
+          engineers, mothers, and fathers, dedicated to the advancement of the
+          Web3.
         </Description>
       </Column>
     </Section1>
-    <Banner2>
-      </Banner2>
-    <Section2>
-      <Column>
-      </Column>
-      <Column>
-      <p>
-      </p>
-      <div> 
-        <iframe
-          src="https://kroniclabz.mypinata.cloud/ipfs/QmUfp6thZQTmNKS6tzijJpxdoBe9X7spHwzRjUh3RPTAwF/token-drop.html?contract=0xE50A259592D3BaBbA4d874Ca3EA87E88BC8b086F&chainId=1&theme=dark"
-          width="1000px"
-          height="600px"
-          Style={"max-width:100%"}
-          frameborder="14"
-        ></iframe>
-      </div>
-      </Column>
-    </Section2>
+    <Banner2></Banner2>
     <Section3>
       <Column>
         <ItemHeading>44k</ItemHeading>
@@ -200,10 +183,10 @@ const Home = () =>
       <Column>
         <Heading>Our Acheivements</Heading>
         <Subheading>
-          Aside from the creation of NFTs, we have mentored over
-          250 creatives in the NFT space!
+          Aside from the creation of NFTs, we have mentored over 250 creatives
+          in the NFT space!
         </Subheading>
-        <Image src={globeImg} width='30vw' height='30vw' />
+        <Image src={globeImg} width="30vw" height="30vw" />
       </Column>
       <Column>
         <ItemHeading>Over 135</ItemHeading>
@@ -213,9 +196,9 @@ const Home = () =>
       </Column>
     </Section3>
     <Section4>
-      <Column>
-      </Column>
+      <Column></Column>
     </Section4>
   </HomeWrapper>
+);
 
-export default Home
+export default Home;
