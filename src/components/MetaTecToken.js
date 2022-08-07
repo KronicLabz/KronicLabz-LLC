1f21df8a2e8692a254a35863dbf2de526242cb84
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-08-06 19:06:39 -0500

    Update MetaTecToken.js

--- src/components/MetaTecToken.js
+++ src/components/MetaTecToken.js
@@ -36,14 +36,13 @@ const MetaTecToken = () => (
     <p></p>
     <Column>
       <Embed>
-        <iframe
-          title="MetaTec Token"
-          src="https://gateway.ipfscdn.io/ipfs/QmUfp6thZQTmNKS6tzijJpxdoBe9X7spHwzRjUh3RPTAwF/token-drop.html?contract=0xE50A259592D3BaBbA4d874Ca3EA87E88BC8b086F&chainId=1&theme=dark"
-          width="600px"
-          height="600px"
-          style="max-width:100%;"
-          frameborder="0"
-        ></iframe>
+       <iframe
+         title="MetaTec Token"
+         src="https://kroniclabz.mypinata.cloud/ipfs/QmUfp6thZQTmNKS6tzijJpxdoBe9X7spHwzRjUh3RPTAwF/token-drop.html?contract=0xE50A259592D3BaBbA4d874Ca3EA87E88BC8b086F&chainId=1&theme=dark"         width="1000px"
+         height="600px"
+         Style={"max-width:100%"}
+         frameborder="14"
+       ></iframe>
       </Embed>
     </Column>
   </MetaTecTokenWrapper>
